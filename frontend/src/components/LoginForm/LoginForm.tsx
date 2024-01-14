9129d661bdf457e7eb948360222e5a6056c8e308
Squarit <marb637@gmail.com>
2024-01-14 22:27:40 +0100

    Frontend files added

---
+++ frontend/src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+interface ILoginFormProps {
+    onSubmit: (username: string, password: string) => void;
+}
+
+const LoginForm: ILoginFormProps = () => {
+    return <form></form>;
+};
