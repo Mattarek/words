e29f9e407e102a8f45676f5b4d14e94a54c5998f
Squarit <marb637@gmail.com>
2024-01-20 00:37:23 +0100

    logInUser function for route added

--- backend/src/controller/authController.ts
+++ backend/src/controller/authController.ts
@@ -1,29 +1,29 @@
 import poolDB from '../db';
 import { Request, Response } from 'express';
-import { FieldPacket, ResultSetHeader } from 'mysql2/promise';
+import { RowDataPacket } from 'mysql2/promise';
 
 export const createUser = async (req: Request, res: Response) => {};
-interface IUser extends ResultSetHeader {
+interface IUser extends RowDataPacket {
     id: number;
     uuid: string;
     login: string;
     email: string;
     password: string;
 }
-
-type UserDataStructure = [IUser, FieldPacket];
-
 export const logInUser = async (req: Request, res: Response) => {
     const { email, password } = req.body;
 
     try {
-        const [rows, fields] = await poolDB.execute(
+        const [userFound] = await poolDB.execute<IUser[]>(
             'SELECT * FROM clients WHERE email = ? AND password = ?',
             [email, password],
         );
-        console.log(rows);
 
-        res.status(200).json('OK');
+        res.status(200).json({
+            login: userFound[0].login,
+            email: userFound[0].email,
+            password: userFound[0].password,
+        });
     } catch (error) {
         console.error(error);
         res.status(500).json({
